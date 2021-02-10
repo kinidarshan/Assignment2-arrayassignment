8dffd37bd10dca26816f3e0409c9e78cbe90c227
Darshan N Kini <78312855+kinidarshan@users.noreply.github.com>
2021-02-10 10:49:55 +0530

    Assignment2-arrayassignment

---
+++ largest_smallest.java
@@ -0,0 +1,17 @@
+package assignment2;
+import java.util.Arrays;
+public class largest_smallest {
+
+	public static void main(String[] args) {
+
+		int arr[] = {55, 10, 8, 90, 43, 87, 95, 25, 50, 12};
+	      System.out.println("Array = "+Arrays.toString(arr));
+	      Arrays.sort(arr);
+	      System.out.println("Sorted Array = "+Arrays.toString(arr));
+	      System.out.println("Smallest element = "+arr[0]);
+	      System.out.println("Second Smallest element = "+arr[1]);
+	      System.out.println("Largest element = "+arr[9]);
+	      System.out.println("Second Largest element = "+arr[8]);
+	}
+
+}
